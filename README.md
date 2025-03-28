1344dba6e711369d7bbad1df26e4762aaed79423
mnguyen1988 <m.nguyen@air-closet.com>
2025-03-29 03:25:46 +0900

    add api and connect api from web

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # KTPM
 project for Software Archiect
+
+
+# START DOCKER
+cd docker
+docker-compose up -d
+
+
+# Run api
+cd kientrucphanmem-api
+yarn install
--- kientrucphanmem-web/src/App.js
+++ kientrucphanmem-web/src/App.js
@@ -1,15 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import { BrowserRouter, Navigate,  Router, Route, Routes } from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
 
-import React from 'react';
-import Login from './Components/Login';
+// import React, { useEffect } from 'react';
+import LoginContainer from './Container/LoginContainer';
+// import Home from './Components/Home';
+// import Register from './Components/Register';
+// import Profile from './Components/Profile';
+import { app_routes } from "./routes";
 
 function App() {
-    return (
-        <div className="App">
-            <Login />
-        </div>
-    );
+    const [render, setRender] = React.useState(false);
+
+    useEffect(() => {
+        console.log('billlllllllllllllllllllllllllllllll');
+        setRender(true);
+    }, []); // [] means this effect will run once after the first render
+
+    const ROUTER_PAGE = (routes) => {
+        console.log('bill-----------', routes);
+        return (
+            <Routes>
+            {routes.map((route, idx) => {
+              return route.component ? (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  element={<route.component />}
+                />
+              ) : null;
+            })}
+            {/* <Navigate from="/" to="/login" /> */}
+          </Routes>
+        );
+    };
+
+
+        // return (
+        //     <BrowserRouter>
+        //       <Routes>
+        //         <Route path="/login" element={<LoginContainer />} />
+        //       </Routes>
+        //     </BrowserRouter>
+        //   );
+
+    if (!render) {
+        return null; // You can return some placeholder
+    } else {
+        return (
+            <BrowserRouter>
+              <React.Suspense 
+            //   fallback={<Loading />}
+              >
+                {ROUTER_PAGE(
+                //   this.props.oauth.logged ? app_logged_routes : app_routes
+                  app_routes
+                )}
+              </React.Suspense>
+          </BrowserRouter>
+        );
+    }
 }
 
 export default App;
