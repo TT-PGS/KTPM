f46b9292e28c90db1b87ce2a8728a6125f4153dd
Tien Truong <thtien.sdh241@hcmut.edu.vn>
2025-04-04 22:08:31 +0700

    Init backend folder
    - Create basic tree of backend folder

---
+++ backend/src/main/java/com/chatmicroservice/common/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.chatmicroservice.common;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    // Getters and Setters
+}
